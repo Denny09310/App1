0a4dd09a0f0323c4795d74fd7d4edd0857830fa0
Koja sig. Dennis <DKOJA@ceia-spa.com>
2024-10-30 13:59:23 +0100

    feat: added BottomSheet control

--- App1/Properties/Elements.cs
+++ App1/Properties/Elements.cs
@@ -1,3 +1,16 @@
 ﻿using BlazorBindings.Maui.ComponentGenerator;
 
+// Icons
 [assembly: GenerateComponentsFromAssembly(typeof(MauiIcons.Core.MauiIcon))]
+
+// Bottom Sheet
+[assembly: GenerateComponent(typeof(The49.Maui.BottomSheet.BottomSheet),
+    Exclude = ["Controller"])]
+
+[assembly: GenerateComponent(typeof(The49.Maui.BottomSheet.Detent))]
+[assembly: GenerateComponent(typeof(The49.Maui.BottomSheet.MediumDetent))]
+[assembly: GenerateComponent(typeof(The49.Maui.BottomSheet.RatioDetent))]
+[assembly: GenerateComponent(typeof(The49.Maui.BottomSheet.HeightDetent))]
+[assembly: GenerateComponent(typeof(The49.Maui.BottomSheet.FullscreenDetent))]
+[assembly: GenerateComponent(typeof(The49.Maui.BottomSheet.AnchorDetent))]
+[assembly: GenerateComponent(typeof(The49.Maui.BottomSheet.ContentDetent))]
